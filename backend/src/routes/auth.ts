1b475f860118f7193824dc7da85d381af0e3ffd2
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-20 13:10:08 +0600

    samesite=none

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -43,7 +43,7 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-
+sameSite:"none",
         maxAge: 24 * 60 * 60 * 1000,
        
       });
