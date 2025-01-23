35820fd0bc02c72cf4210b781eafd7a459a2cccf
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-23 13:53:38 +0600

    err:fix bux on starRatingFilter and HotelTyesFilter

--- frontend/src/components/StarRatingFilter.tsx
+++ frontend/src/components/StarRatingFilter.tsx
@@ -10,7 +10,7 @@ const StarRatingFilter = ({ selectedStars, onChange }: Props) => {
     <div className="border-b border-slate-300 pb-5">
       <h4 className="text-md font-semibold mb-2">Property Rating</h4>
       {["1", "2", "3", "4", "5"].map((star) => (
-        <label className="flex items-center space-x-2">
+        <label key={star} className="flex items-center space-x-2">
           <input
             type="checkbox"
             className="rounded"
