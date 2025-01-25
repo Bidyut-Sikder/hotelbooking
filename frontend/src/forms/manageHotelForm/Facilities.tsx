0c0c16e3d7631a996751740dfe0b5fdcc94e7b37
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-25 21:36:41 +0600

    fix: add/edid form facilities part's responsiveness done

--- frontend/src/forms/manageHotelForm/Facilities.tsx
+++ frontend/src/forms/manageHotelForm/Facilities.tsx
@@ -9,12 +9,46 @@ function Facilities() {
     register,
   } = useFormContext<HotelFormData>();
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-3">Facilities</h2>
+    // <div>
+    //   <h2 className="text-2xl font-bold mb-3">Facilities</h2>
 
-      <div className="grid grid-cols-5 gap-3">
+    //   <div className="grid grid-cols-5 gap-3">
+    //     {hotelFacilities.map((facility) => (
+    //   <label key={facility} className="text-sm flex gap-1 text-gray-700">
+    //     <input
+    //       {...register("facilities", {
+    //         validate: (facilities) => {
+    //           if (facilities && facilities.length > 0) {
+    //             return true;
+    //           } else {
+    //             return "Please select at least one facility";
+    //           }
+    //         },
+    //       })}
+    //       type="checkbox"
+    //       value={facility}
+    //     />
+    //     {facility}
+    //   </label>
+    // ))}
+    //   </div>
+    //   {errors.facilities && (
+    //     <span className="text-red-500 text-sm font-bold">
+    //       {errors.facilities.message}
+    //     </span>
+    //   )}
+    // </div>
+
+    <div>
+      <h2 className="text-xl sm:text-2xl font-bold mb-2 text-center sm:text-left">
+        Facilities
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
         {hotelFacilities.map((facility) => (
-          <label key={facility} className="text-sm flex gap-1 text-gray-700">
+          <label
+            key={facility}
+            className="text-xs sm:text-sm flex gap-1 text-gray-700 items-center"
+          >
             <input
               {...register("facilities", {
                 validate: (facilities) => {
@@ -25,6 +59,7 @@ function Facilities() {
                   }
                 },
               })}
+              className="h-4 w-4"
               type="checkbox"
               value={facility}
             />
