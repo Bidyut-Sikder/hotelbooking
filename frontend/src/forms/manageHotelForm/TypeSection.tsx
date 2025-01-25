c11691f88d250600a88803fceac33466640726a4
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-25 21:27:37 +0600

    fix: typesection responsiveness done

--- frontend/src/forms/manageHotelForm/TypeSection.tsx
+++ frontend/src/forms/manageHotelForm/TypeSection.tsx
@@ -11,16 +11,45 @@ function TypeSection() {
   const typeWatch = watch("type");
   // console.log(typeWatch);
   return (
+    // <div>
+    //   <h2 className="text-2xl font-bold mb-3">Type</h2>
+    //   <div className="grid grid-cols-5 gap-2">
+    //   {hotelTypes.map((type) => (
+    //     <label
+    //       key={type}
+    //       className={
+    //         typeWatch === type
+    //           ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
+    //           : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold "
+    //       }
+    //     >
+    //       <input
+    //         type="radio"
+    //         value={type}
+    //         {...register("type", { required: "This field is required" })}
+    //         className="hidden"
+    //       />
+    //       <span>{type}</span>
+    //     </label>
+    //   ))}
+    // </div>
+    // {errors.type && (
+    //   <span className="text-red-500">{errors.type.message}</span>
+    // )}
+    // </div>
+
     <div>
-      <h2 className="text-2xl font-bold mb-3">Type</h2>
-      <div className="grid grid-cols-5 gap-2">
+      <h2 className="text-xl sm:text-2xl font-bold mb-2 text-center sm:text-left">
+        Type
+      </h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-2">
         {hotelTypes.map((type) => (
           <label
             key={type}
             className={
               typeWatch === type
-                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
-                : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold "
+                ? "cursor-pointer bg-blue-300 text-xs sm:text-sm rounded-full px-3 py-1.5 font-semibold"
+                : "cursor-pointer bg-gray-300 text-xs sm:text-sm rounded-full px-3 py-1.5 font-semibold"
             }
           >
             <input
--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -14,9 +14,14 @@ const MyHotels = () => {
   return (
     <div className="space-y-5">
       <span className="flex justify-between">
-        <h1 className="text-3xl font-bold">My Hotels</h1>
+        {/* <h1 className="text-3xl font-bold">My Hotels</h1> */}
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center sm:text-left">
+          My Hotels
+        </h1>
+
         <Link
-          className="flex bg-blue-600 text-white text-xl p-2 hover:bg-blue-500 font-bold"
+          className="hover:bg-blue-500 flex bg-blue-600 font-bold items-center text-white px-3 py-1 text-sm sm:text-base rounded hover:text-green-500"
+          // className="flex bg-blue-600 text-white text-xl p-2 hover:bg-blue-500 font-bold"
           to="/add-hotel"
         >
           Add Hotel
@@ -31,7 +36,7 @@ const MyHotels = () => {
             >
               <h3 className="text-2xl font-bold"> {hotel.name}</h3>
               <div className="whitespace-pre-line"> {hotel.description}</div>
-              <div className="grid grid-cols-5 gap-2">
+              {/* <div className="grid grid-cols-5 gap-2">
                 <div className="border border-slate-300 rounded-sm p-3 flex items-center">
                   <BsMap className="mr-1" />
                   {hotel.city},{" "}
@@ -53,10 +58,34 @@ const MyHotels = () => {
                   <BiStar className="mr-1" />
                   {hotel.starRating} Star Rating
                 </div>
+              </div> */}
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-2 p-2">
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center text-sm">
+                  <BsMap className="mr-1" />
+                  {hotel.city}, {hotel.country}
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center text-sm">
+                  <BsBuilding className="mr-1" />
+                  {hotel.type}
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center text-sm">
+                  <BiMoney className="mr-1" />${hotel.pricePerNight}
+                  $200
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center text-sm">
+                  <BiHotel className="mr-1" />
+                  {hotel.adultCount} adults, {hotel.childCount} children
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center text-sm">
+                  <BiStar className="mr-1" /> {hotel.starRating} Star Rating
+                </div>
               </div>
+
               <span className="flex justify-end">
                 <Link
-                  className="flex bg-blue-600 text-white text-xl p-2 hover:bg-blue-500 font-bold"
+                  className="hover:bg-blue-500 flex bg-blue-600 font-bold items-center text-white px-3 py-1 text-sm sm:text-base rounded hover:text-green-500"
+                  // className="flex bg-blue-600 text-white text-xl p-2 hover:bg-blue-500 font-bold"
                   to={`/edit-hotel/${hotel._id}`}
                 >
                   View Details
